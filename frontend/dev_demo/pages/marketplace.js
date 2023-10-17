3fcae8069e85cb5fdb102773f51bcad54699ec72
aabdel0181 <ahmedabdellahhoco@gmail.com>
2023-10-17 18:01:16 -0400

    Updated spacing & Nav - Marketplace

--- frontend/dev_demo/pages/marketplace.js
+++ frontend/dev_demo/pages/marketplace.js
@@ -164,6 +164,7 @@ function Marketplace() {
     width: "100px",
     textAlign: "right",
   };
+
   return (
     <div className="bg-gray-100 min-h-screen p-5">
       <h1 className="text-4xl font-bold mb-5">Marketplace</h1>
@@ -173,34 +174,40 @@ function Marketplace() {
       {collections.map((collection) => (
         <div
           key={collection.id}
-          className="bg-white p-6 rounded-xl shadow-lg mb-6"
+          className="bg-white p-6 rounded-xl shadow-lg mb-6 relative"
         >
           <h2 className="text-3xl mb-4">{collection.title}</h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {collection.nfts.map((nft) => (
-              <Link
-                href={`/nft/${nft.id}`}
-                key={nft.id}
-                className="group border w-64 rounded-lg overflow-hidden hover:shadow-lg transition-shadow"
-              >
-                <div>
-                  <img
-                    className="w-full max-h-60 object-cover mb-4"
-                    src={nft.image}
-                    alt={nft.title}
-                  />
-                  <div className="p-4">
-                    <h3 className="text-xl">{nft.title}</h3>
-                    <p className="text-gray-600 mt-2 hidden group-hover:block">
-                      {nft.description}
-                    </p>
-                    <span className="text-green-500 mt-2 hidden group-hover:block">
-                      {nft.price}
-                    </span>
+
+          <button className="absolute top-1/2 left-2 z-10">←</button>
+          <button className="absolute top-1/2 right-2 z-10">→</button>
+
+          <div className="overflow-x-auto">
+            <div className="flex flex-row gap-2">
+              {collection.nfts.map((nft) => (
+                <Link
+                  href={`/nft/${nft.id}`}
+                  key={nft.id}
+                  className="group border w-64 rounded-lg overflow-hidden hover:shadow-lg transition-shadow"
+                >
+                  <div>
+                    <img
+                      className="w-full max-h-60 object-cover mb-4"
+                      src={nft.image}
+                      alt={nft.title}
+                    />
+                    <div className="p-4">
+                      <h3 className="text-xl">{nft.title}</h3>
+                      <p className="text-gray-600 mt-2 hidden group-hover:block">
+                        {nft.description}
+                      </p>
+                      <span className="text-green-500 mt-2 hidden group-hover:block">
+                        {nft.price}
+                      </span>
+                    </div>
                   </div>
-                </div>
-              </Link>
-            ))}
+                </Link>
+              ))}
+            </div>
           </div>
         </div>
       ))}
